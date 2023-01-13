f5754f633133f3c7953e604445d6ca3da7f97f02
Zalman Izak <thezalmanian@gmail.com>
2023-01-12 17:56:11 -0800

    Added method to post student data to database - Changed Student dob input type to date-only

--- MVCModelBinding/Controllers/StudentController.cs
+++ MVCModelBinding/Controllers/StudentController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCModelBinding.Models;
 
 namespace MVCModelBinding.Controllers
 {
     public class StudentController : Controller
     {
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student currStudent)
+        {
+            // If all data is valid
+            if (ModelState.IsValid)
+            {
+                // Add student to database
+
+                // Send user back to home page
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If all data is not valid, show user
+            // same page- with errors
+            return View(currStudent);
         }
     }
 }
