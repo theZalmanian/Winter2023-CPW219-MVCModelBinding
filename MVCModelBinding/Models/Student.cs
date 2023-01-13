159515edf2c2dd3a8234da8a9b833618549166e4
Zalman Izak <thezalmanian@gmail.com>
2023-01-12 16:52:23 -0800

    Added required fields to student Class

--- MVCModelBinding/Models/Student.cs
+++ MVCModelBinding/Models/Student.cs
@@ -5,5 +5,19 @@
     /// </summary>
     public class Student
     {
+        /// <summary>
+        /// The student's legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The student's legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The student's date of birth
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
     }
 }
