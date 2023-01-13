fbdc4da9adf15c8e31bfdf468c139bfe1850df95
Zalman Izak <thezalmanian@gmail.com>
2023-01-12 17:09:58 -0800

    Changed the student class field's display names and error message text

--- MVCModelBinding/Models/Student.cs
+++ MVCModelBinding/Models/Student.cs
@@ -1,4 +1,6 @@
-﻿namespace MVCModelBinding.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCModelBinding.Models
 {
     /// <summary>
     /// Represents an individual college student
@@ -8,16 +10,22 @@
         /// <summary>
         /// The student's legal first name
         /// </summary>
+        [Required(ErrorMessage = "First name is required!")]
+        [Display(Name = "First Name:")]
         public string FirstName { get; set; }
 
         /// <summary>
         /// The student's legal last name
         /// </summary>
+        [Required(ErrorMessage = "Last name is required!")]
+        [Display(Name = "Last Name:")]
         public string LastName { get; set; }
 
         /// <summary>
         /// The student's date of birth
         /// </summary>
+        [Required(ErrorMessage = "Date of birth is required!")]
+        [Display(Name = "Date of Birth:")]
         public DateOnly DateOfBirth { get; set; }
     }
 }
